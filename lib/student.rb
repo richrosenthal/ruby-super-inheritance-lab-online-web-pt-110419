956df53c403e4d8a62ae1d915d06e682fdeec7f2
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-15 17:20:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,11 @@ class Student
   
   
   def hello
-   puts "Hey there! I'm so excited to learn stuff"
+   puts "Hey there! I'm so excited to learn stuff."
   end 
   
   def raise_hand 
-   puts "Pick me"
+   puts "Pick me!"
   end 
   
   
